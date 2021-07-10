58c1dd7f1460c37065307361debc8100630f9aab
seoyeon <silversun0808@gmail.com>
2021-07-10 20:38:21 +0900

    modify #1065

--- Algorithm_Study/backjoon/level 6/1065.py
+++ Algorithm_Study/backjoon/level 6/1065.py
@@ -1,11 +1,12 @@
 num = int(input())
-hansu = 0
+han = 0
 
 for n in range(1, num+1) :
-  if n <= 99 : # 1부터 99까지는 모두 한수
-    hansu += 1 
+  if n < 100 : # 1부터 99까지는 모두 한수
+    han += 1 
     
   else :     
     nums = list(map(int, str(n))) # 숫자를 자릿수대로 분리 
     if nums[0] - nums[1] == nums[1] - nums[2] : #등차수열 확인
-      hansu+=1
+      han+=1
+print(han)
