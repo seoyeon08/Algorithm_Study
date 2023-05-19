82fb99094f1b3f7cba5f3b5c16297e55d384add8
seoyeon08 <silversun0808@gmail.com>
2023-05-20 00:35:52 +0900

    [Add] 짝지어 제거하기.py

---
+++ 짝지어 제거하기.py
@@ -0,0 +1,14 @@
+def solution(s):
+    answer = 0
+    stack = []
+    for i in s :
+        if len(stack) == 0 :
+            stack.append(i)
+        else :
+            if stack[-1] == i : 
+                stack.pop()
+            else :
+                stack.append(i)
+    if len(stack) == 0 :
+        answer = 1
+    return answer
