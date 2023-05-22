5496e95e4910fe9441d55437ce43f0143b36bb0c
seoyeon08 <silversun0808@gmail.com>
2023-05-23 00:08:36 +0900

    [Add] 구명보트.py

---
+++ programmers/Lev2/구명보트.py
@@ -0,0 +1,16 @@
+# deque 안쓰면 효율성 검사 1번 불합.. 쓰면 통과
+from collections import deque
+def solution(people, limit):
+    answer = 0
+    people = deque(sorted(people))
+    while people :
+        if len(people) == 1 :
+            answer += 1
+            break
+        if people[0] + people[-1] <= limit :
+            people.pop()
+            people.popleft()
+        else :
+            people.pop()
+        answer += 1
+    return answer
